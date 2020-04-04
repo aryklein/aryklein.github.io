333b547051647f55aff063f545c80614001b90a7
Ary Kleinerman <kleinerman@gmail.com>
2020-04-04 18:10:14 -0300

    Change blog title
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Ary Kleinerman"  # (translatable)
-BLOG_TITLE = "Ary's geek blog"  # (translatable)
+BLOG_TITLE = "Chronicles of a Sysadmin"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://ary.kleinerman.org/"
